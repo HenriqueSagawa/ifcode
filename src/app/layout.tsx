7bc446f07780f8adad7f0620042644d1dc3c06a6
jaovct <151976857+jaovct@users.noreply.github.com>
2025-02-27 00:26:18 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,23 +5,32 @@ import { Navbar } from "@/components/Navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Footer } from "@/components/Footer";
 
-
+// Define a fonte Geist Sans como uma fonte local.
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
+// Define a fonte Geist Mono como uma fonte local.
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
 
+// Define os metadados da aplicação.
 export const metadata: Metadata = {
   title: "IF Code",
   description: "",
 };
 
+/**
+ * Componente que define o layout raiz da aplicação.
+ * @param {Readonly<{ children: React.ReactNode; }>} props - As propriedades do componente.
+ * @param {React.ReactNode} props.children - O conteúdo a ser renderizado dentro do layout.
+ * @returns {JSX.Element} O layout da aplicação.
+ */
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -30,20 +39,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col`}>
-
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
-          {/* <head>
-            <link rel="icon" href={favicon.src} color="#fff" />
-          </head> */}
           <Navbar />
-
           {children}
-
           <Footer />
         </ThemeProvider>
       </body>
