e53077a474591b00d79aeedf92f2dab505f25362
jaovct <151976857+jaovct@users.noreply.github.com>
2025-02-27 00:36:49 -0300

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,11 @@
+/**
+ * Componente que renderiza o rodapé (footer) da aplicação.
+ *
+ * Este componente exibe uma mensagem de copyright e informa que foi desenvolvido
+ * com café pela equipe do IF Code.
+ *
+ * @returns {JSX.Element} O rodapé renderizado.
+ */
 export function Footer() {
   return (
     <footer className="flex-grow flex justify-center items-center text-gray-600 w-full h-24">
